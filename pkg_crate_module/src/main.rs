1af08e474f3329787038b436f3b289e453c600a5
captainlee1024 <644052732@qq.com>
2021-04-17 20:07:23 +0800

    added pkg_crate_module

--- pkg_crate_module/src/main.rs
+++ pkg_crate_module/src/main.rs
@@ -1,3 +1,18 @@
+// use std::cmp::Ordering;
+// use std::io;
+// same the following
+use std::{cmp::Ordering, io};
 fn main() {
     println!("Hello, world!");
+    // Managing Growing Projects With Packages, Crates, and Modules
+
+    //Packages and Crate
+    // cargo new project-name: When we entered the command, Cargo created a Cargo.toml file
+
+    //Defining Modules to Control Scope and Privacy
+
+    // Using Nested Paths to clean Up Large use Lists
+
+    // Use the Glob Operator
+    // eg: std::collections::*;
 }
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -5,17 +5,37 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+mod back_of_house;
+mod front_of_house;
+// use + Absolute path
+// use crate::front_of_house::hosting;
+// Re-exporting Names with pub use
+pub use crate::front_of_house::hosting;
 
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+// use + Relative path
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
 
-        fn seat_at_table() {}
-    }
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 
-    mod serving {
-        fn take_order() {}
-        fn server_order() {}
-        fn take_payment() {}
-    }
+    // use
+    hosting::add_to_waitlist();
+}
+
+fn serve_order() {}
+
+pub fn eat_at_restaurant2() {
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+}
+
+pub fn eat_at_restaurant3() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+    println!("{:#?}, {:#?}", order1, order2);
 }
